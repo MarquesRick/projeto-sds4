ce1aa4ffc7abe7366402134c3c8fa831cdf38b48
Henrique Marques <37425086+MarquesRick@users.noreply.github.com>
2021-09-08 23:42:05 -0300

    Pagination

---
+++ backend/src/main/java/com/devsuperior/dsvendas/service/SaleService.java
@@ -0,0 +1,36 @@
+package com.devsuperior.dsvendas.service;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.dsvendas.dto.SaleDTO;
+import com.devsuperior.dsvendas.entities.Sale;
+import com.devsuperior.dsvendas.repositories.SaleRepository;
+import com.devsuperior.dsvendas.repositories.SellerRepository;
+
+@Service
+public class SaleService {
+	
+	//Instancia injetada automaticamente pelo framework
+	@Autowired
+	private SaleRepository repository;
+	
+	@Autowired
+	private SellerRepository sellerRepository;
+	
+	//essa annotation garante que toda operação com o banco seja resolvido nessa function
+	//readOnly para evitar Lock no banco
+	@Transactional(readOnly = true)
+	public Page<SaleDTO> findAll(Pageable pageable){
+		//forma para buscar os vendedores, JPA salvar em memória para não ficar consultando uma a uma 
+		//recomendável utilizar apenas em tabelas com poucos registros < 1000 registros
+		sellerRepository.findAll();
+		Page<Sale> result = repository.findAll(pageable);
+		return result.map(x -> new SaleDTO(x));
+	}
+
+}
